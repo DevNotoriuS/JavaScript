79387f6d0464c7f903556c60e6eb84c454f3fdf7
DevNotoriuS <164415661+DevNotoriuS@users.noreply.github.com>
2024-05-15 17:38:34 -0300

    exercícios

---
+++ aula12/ex012.js
@@ -0,0 +1,9 @@
+var dinheiro = 1800
+
+if (dinheiro < 1200) {
+    console.log('Você está fálido!')
+} else if (dinheiro > 1200 || dinheiro < 1400) {
+    console.log('Você está estável!')
+} else {
+    console.log('Você tem muita grana!')
+}
